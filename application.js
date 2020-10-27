f1d608e01f7b69f56f995dc079b3adf82da442c8
adam <dev@adamjayne.org>
2020-10-27 17:31:56 -0400

    first build

--- application.js
+++ application.js
@@ -3,15 +3,13 @@ const { app, BrowserWindow } = require("electron");
 function createWindow() {
   const win = new BrowserWindow({
     width: 400,
-    height: 680,
+    height: 685,
     webPreferences: {
       nodeIntegration: true,
     },
     title: "Eleven Fifty Academy System Detection",
   });
-
   win.loadFile("./src/views/index.html");
-  win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
